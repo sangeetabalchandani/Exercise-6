272ce47fa057dbffffd261277241e82ef13b62f2
sangeetabalchandani <balchandanisangeeta14@gmail.com>
2021-06-08 14:52:08 -0400

    task1.py updated

---
+++ task1.py
@@ -0,0 +1,11 @@
+counter = 1
+while counter <= 5:
+    number = int(input("Guess the " + str(counter) + " number "))
+    if number != 5:
+        print ("Try again.")
+    else:
+        print ("Good guess!")
+        break
+    counter = counter +1
+else:
+    print ("Sorry but that was not very successful")
